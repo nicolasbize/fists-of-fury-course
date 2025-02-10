2e19f056012fa6751ecaf51fa1bbd49adb81eea6
Nicolas Bize <nicolasbize@gmail.com>
2025-02-09 20:39:13 -0800

    E03: Collision Detection

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -6,10 +6,14 @@ extends CharacterBody2D
 
 @onready var animation_player := $AnimationPlayer
 @onready var character_sprite := $CharacterSprite
+@onready var damage_emitter := $DamageEmitter
 
-enum State {IDLE, WALK}
+enum State {IDLE, WALK, ATTACK}
 
 var state := State.IDLE
+
+func _ready() -> void:
+	damage_emitter.area_entered.connect(on_emit_damage.bind())
 
 func _process(_delta: float) -> void:
 	handle_input()
@@ -19,23 +23,46 @@ func _process(_delta: float) -> void:
 	move_and_slide()
 
 func handle_movement() -> void:
-	if velocity.length() == 0:
-		state = State.IDLE
+	if can_move():
+		if velocity.length() == 0:
+			state = State.IDLE
+		else:
+			state = State.WALK
 	else:
-		state = State.WALK
+		velocity = Vector2.ZERO
 
 func handle_input() -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
+	if can_attack() and Input.is_action_just_pressed("attack"):
+		state = State.ATTACK
 
 func handle_animations() -> void:
 	if state == State.IDLE:
 		animation_player.play("idle")
 	elif state == State.WALK:
 		animation_player.play("walk")
+	elif state == State.ATTACK:
+		animation_player.play("punch")
 
 func flip_sprites() -> void:
 	if velocity.x > 0:
 		character_sprite.flip_h = false
+		damage_emitter.scale.x = 1
 	elif velocity.x < 0:
 		character_sprite.flip_h = true
+		damage_emitter.scale.x = -1
+
+func can_move() -> bool:
+	return state == State.IDLE or state == State.WALK
+
+func can_attack() -> bool:
+	return state == State.IDLE or state == State.ATTACK
+
+func on_action_complete() -> void:
+	state = State.IDLE
+
+func on_emit_damage(damage_receiver: DamageReceiver) -> void:
+	var direction := Vector2.LEFT if damage_receiver.global_position.x < global_position.x else Vector2.RIGHT
+	damage_receiver.damage_received.emit(damage, direction)
+	print(damage_receiver)
